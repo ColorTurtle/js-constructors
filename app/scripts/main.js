7daf3ff6261d89388d126df69046ca961b66ec34
Daniel Donaldson <donaldson.daniel@gmail.com>
2014-01-27 17:27:22 -0500

    Created constructors for AptUnit, CreateCharacter, & CreateZombie

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,67 @@
-console.log('\'Allo \'Allo!');
+console.log('Hello there, thanks for checking on my program!');
+console.log('To start off, lets say you are an appartment owner.');
+
+
+
+function AptUnit (unit, tenant){
+	this.unitNumber = unit;
+	this.renter = tenant;
+	this.occuppied = true;
+	this.latePayments = 0;
+	this.sendEviction = false;
+	this.zombieProof = true;
+	this.accountStatus = 'Up to date.';
+	this.rentLate = function() {
+		this.accountStatus = 'Rent past due.';
+		this.latePayments = ++this.latePayments;
+		return 'Renter ' + this.renter + ' is past due on Unit ' + this.unitNumber + '.'
+	};
+	this.paidup = function(){
+		this.accountStatus = 'Up to date.';
+		return 'Renter ' + this.renter + ' is now up to date on Unit ' + this.unitNumber + '.'
+	}
+};
+
+var blueHouse = new AptUnit(103, 'Betty White');
+
+var redHouse = new AptUnit(105, 'Mr. Wilson');
+
+
+function CreateCharacter (name, age){
+	this.name = name;
+	this.age = age;
+	this.alive = true;
+	this.walker = false;
+	this.headless = false;
+	this.nordicKill = function (zombie) {
+		zombie.headless = true;
+		zombie.walker = false;
+	}
+	this.breakDoor = function (aptName) {
+		aptName.zombieProof = false;
+		return 'Somebody better patch ' + aptName.unitNumber + '\'s door with the \'patch\' function.'
+	}
+
+	this.patch = function (aptName) {
+		aptName.zombieProof = true;
+		return 'Whew! That was close!'
+	}
+};
+
+var joe = new CreateCharacter ('Joe', 26);
+
+function CreateZombie (name){
+	this.name = name;
+	this.alive = false;
+	this.walker = true;
+	this.headless = false;
+	this.breakDoor = function (aptName) {
+		aptName.zombieProof = false;
+		return 'Somebody better patch ' + aptName.unitNumber + '\'s door with the \'patch\' function.'
+	}
+
+	this.patch = function (aptName) {
+		return 'What the heck!?!? Zombies don\'t fix doors!'}
+};
+
+var zombieFred = new CreateZombie ('Fred');
